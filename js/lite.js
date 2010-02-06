c817ff7e6274e13994260e2da3373ae70edef886
Nadir Kadem <nadir.kadem@dailymotion.com>
2010-02-06 01:10:12 +0100

    add skinning, use playerv3 instead of v4, make contexts works

--- js/lite.js
+++ js/lite.js
@@ -34,89 +34,226 @@
   };
 })();
 
-
-function displayResults(videos)
+Lite =
 {
-    $('#results').fadeIn('slow');
-    var res = '';
-    if (videos.length == 0)
+    player: null,
+    currentColor: null,
+    foregroundColor: '#FFFFFF',
+    noColor: 'rgba(255, 255, 255, 0.5)',
+    playerDisplayed: false,
+    displayResults: function(videos)
     {
-        res = 'No videos found';
-    }
-    else
-    {
-        for (i in videos)
+        $('#results').fadeIn('slow');
+        var res = '';
+        if (videos.length == 0)
         {
-            res += tmpl("video_tmpl", videos[i]);
+            res = 'No videos found';
         }
-    }
-    $('#results').html(res);
-    $('.video_item').click(function(){
-       player.loadVideoById($(this).attr('id'));
-    });
-}
+        else
+        {
+            for (i in videos)
+            {
+                res += tmpl("video_tmpl", videos[i]);
+            }
+        }
+        $('#results').html(res);
+        
+        if ($('embed').size() == 0 && !Lite.playerDisplayed)
+        {
+            Lite.playerDisplayed = true;
+            Lite.loadVideoById($('.video_item:first').attr('id'));
+        }
 
-function search()
-{
-    var req = '/search/' + $('#search').val() + '/1:30';
-    $('nav#tabs').removeClass('selected');
-    $('nav#tabs #recent_searches').addClass('selected');
-
-    if ($('a[href="' + req + '"]').size() == 0)
+        $('.video_item').click(function(){
+           Lite.loadVideoById($(this).attr('id'));
+        });
+    },
+    search: function()
     {
-        request(req);
-        $('nav#buttons').append('<a href="' + req + '">search: ' + $('#search').val() + '<span class="delete"></span></a>');
-        $('nav#buttons a:last').hide().fadeIn().addClass('selected');
-        initNav();
-    }
-    else
+        var req = '/search/' + $('#search').val() + '/1:30';
+        $('nav#tabs').removeClass('selected');
+        $('nav#tabs #searches').addClass('selected');
+
+        if ($('a[href="' + req + '"]').size() == 0)
+        {
+            if($('nav#buttons #nav_searches a').size() == 0)
+            {
+                $('nav#buttons #nav_searches').html('');
+            }
+            Lite.request(req);
+            $('nav#buttons #nav_searches').append('<a href="' + req + '" class="search selected">search: ' + $('#search').val() + '<span class="delete"></span></a>');
+            $('nav#buttons a:last').hide().fadeIn().addClass('selected');
+            Lite.initNav();
+        }
+        else
+        {
+            $('a[href="' + req + '"]').fadeOut('fast', function(){$(this).fadeIn();}).addClass('selected');
+        }
+        Lite.goToContext('nav_searches');
+        Lite.applyColors();
+    },
+    request: function(req)
     {
-        $('a[href="' + req + '"]').fadeOut('fast', function(){$(this).fadeIn()}).addClass('selected');
+        $('#results').fadeOut('fast');
+        var req = 'http://www.dailymotion.com/json' + req + '?callback=Lite.displayResults&fields=title,thumbnail_small_url,video_id';
+        $('#results').append('<scri' + 'pt src="' + req + '"></scri' + 'pt>');
+        Lite.applyColors();
+    },
+    initNav: function()
+    {
+        $('nav#tabs a').click(function(){
+            $('nav#tabs a').removeClass('selected');
+            $(this).addClass('selected');
+            Lite.goToContext('nav_' + $(this).attr('id'));
+            if ($(this).attr('id') == 'history')
+            {
+                Lite.request('/user/Youpinadi/1:30');
+            }
+            Lite.applyColors();
+            return false;
+        });
+
+        $('nav#buttons a').click(function(){
+            $('nav#buttons a.selected').css('color', Lite.foregroundColor);
+            $('nav#buttons a').removeClass('selected');
+            $(this).addClass('selected');
+            Lite.request($(this).attr('href'));
+            return false;
+        }).find('.delete').click(function()
+        { 
+            $(this).parent().remove();
+            if($('nav#buttons #nav_searches a').size() == 0)
+            {
+                $('nav#buttons #nav_searches').html('no recent search');
+            }
+            return false;
+        });
+    },
+    applyColors: function()
+    {
+        //RESET TABS COLORS
+        $('#tabs a')
+            .css('backgroundColor', '#000000')
+            .css('color', Lite.foregroundColor);
+
+        $('#tabs a.selected')
+            .css('backgroundColor', Lite.currentColor);
+
+        //RESET BUTTON COLORS
+        $('#buttons a')
+            .css('backgroundColor', Lite.noColor)
+            .css('color', Lite.foregroundColor);
+
+        $('#buttons a.selected')
+            .css('backgroundColor', Lite.foregroundColor)
+            .css('color', Lite.currentColor);
+
+        $('.background').css('backgroundColor', Lite.currentColor);
+
+        $('#buttons a').hover(
+          function () {
+            $(this)
+                .css('backgroundColor', Lite.foregroundColor)
+                .css('color', Lite.currentColor);
+          },
+          function () {
+              if (!$(this).hasClass('selected'))
+              {
+                  $(this)
+                    .css('backgroundColor', Lite.noColor)
+                    .css('color', Lite.foregroundColor);
+              }
+          }
+        );
+    },
+    goToContext: function(destContext)
+    {
+        if ($('#buttons .current_context').attr('id') == destContext)
+        {
+            return;
+        }
+
+        var out = $('#buttons .current_context').attr('id');
+
+        $('#buttons .current_context').removeClass('current_context');
+        $('#buttons #' + destContext)
+            .addClass('current_context')
+            .css('top', 40);
+
+        $('#buttons #' + out)
+            .animate
+            (
+                {
+                    'top': -38
+                },
+                'fast',
+                function()
+                {
+                    $('#buttons #' + destContext)
+                        .animate
+                        (
+                            {
+                                'top': 0
+                            }
+                        );
+                }
+            );
+
+    },
+    rgbToHexa: function(rgbString)
+    {
+        var parts = rgbString
+                .match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/)
+        ;
+        // parts now should be ["rgb(0, 70, 255", "0", "70", "255"]
+
+        delete (parts[0]);
+        for (var i = 1; i <= 3; ++i) {
+            parts[i] = parseInt(parts[i]).toString(16);
+            if (parts[i].length == 1) parts[i] = '0' + parts[i];
+        }
+        return parts.join(''); // "0070ff"
+    },
+    loadVideoById: function(videoId)
+    {
+        var col = Lite.rgbToHexa(Lite.currentColor);
+        var cols = 'colors=background:000000;glow:000000;foreground:cccccc;special:' + col +';&autoPlay=1';
+        var flashvars = {};
+        var params = {'allowscriptaccess': 'always'};
+        var attributes = {};
+        swfobject.embedSWF("http://www.dailymotion.com/swf/" + videoId + '?' + cols, "video_player", "300", "120", "9.0.0","expressInstall.swf", flashvars, params, attributes);
+    },
+    switchColors: function(event)
+    {
+        var el = $(event.target);
+        Lite.currentColor = el.css('backgroundColor');
+        $('#colors span').removeClass('selected');
+        $(el).addClass('selected');
+        $('.background').css('backgroundColor', Lite.currentColor);
+        Lite.applyColors();
+    },
+    initialize: function()
+    {
+        $(document).ready(function () {
+            Lite.currentColor = $('#colors span.selected').css('backgroundColor');
+            Lite.applyColors();
+            $('#colors span').click(Lite.switchColors);
+
+            $('#search_form').submit(function(){
+                $('nav a').removeClass('selected');
+                Lite.search();
+                return false;
+            });
+            Lite.initNav();
+            Lite.request('/buzz/1:30');
+        });
     }
-}
+};
+Lite.initialize();
 
-function request(req)
-{
-    $('#results').fadeOut('fast');
-    var req = 'http://www.dailymotion.com/json' + req + '?callback=displayResults&fields=title,thumbnail_small_url,video_id';
-    $('#results').append('<scri' + 'pt src="' + req + '"></scri' + 'pt>');
-}
 
-var player;
 function onDailymotionPlayerReady(id)
 {
-    player = $('#video_player').get(0);
+    console.log('OK');
+    Lite.player = $(id);
 };
-
-function initNav()
-{
-    $('nav#tabs a').click(function(){
-        $('nav#tabs a').removeClass('selected');
-        $(this).addClass('selected');
-        return false;
-    });
-
-    $('nav#buttons a').click(function(){
-        $('nav#buttons a').removeClass('selected');
-        $(this).addClass('selected');
-        request($(this).attr('href'));
-        return false;
-    }).find('.delete').click(function(){ $(this).parent().fadeOut('fast'); return false});
-}
-
-$(document).ready(function () {
-
-    var flashvars = {'enableApi' : 1};
-    var params = {'allowscriptaccess': 'always'};
-    var attributes = {};
-    swfobject.embedSWF("http://stage-06.dailymotion.com/apiplayer?enableApi=1", "video_player", "300", "120", "9.0.0","expressInstall.swf", flashvars, params, attributes);
-
-
-    $('#search_form').submit(function(){
-        $('nav a').removeClass('selected');
-        search();
-        return false;
-    });
-    initNav();
-    request('/buzz/1:30');
-});
